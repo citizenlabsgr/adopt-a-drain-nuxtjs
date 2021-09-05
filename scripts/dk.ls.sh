84464698fb262feb009c29f1b762f408370ed65b
James <wilfongjt@gmail.com>
2021-09-05 08:05:43 -0400

    29.docker.add.api

---
+++ scripts/dk.ls.sh
@@ -0,0 +1,11 @@
+cd .. 
+#docker image inspect "citizenlabsgr/adopt-a-drain-hapi-api"
+#docker image inspect IMAGE 6d2b02d92d11
+#docker image inspect "citizenlabsgr/adopt-a-drain-hapi-api"
+#docker image inspect citizenlabsgr/adopt-a-drain-hapi-api
+
+#citizenlabsgr/adopt-a-drain-hapi-api
+
+docker-compose run db-api ls /usr/src/api
+
+#docker-compose run db-api ls

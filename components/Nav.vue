71d8b4ee2c364c5f84aa99919686441943020a5e
James <wilfongjt@gmail.com>
2021-11-30 16:46:48 -0500

    Update Nav.vue

--- components/Nav.vue
+++ components/Nav.vue
@@ -34,9 +34,9 @@
 </template>
 
 <script>
-// import { Token Helper } from '@/components/mixins/Token Helper.js'
+
 import Expiration from '@/components/mixins/ExpirationMixin.js'
-/* istanbul ignore next */ 
+
 export default {
   mixins: [Expiration],
   data () {
@@ -48,34 +48,12 @@ export default {
   computed: {
     salutation () {
       if (this.$store.state.authenticated) {
-        return `xxx this.$store.state.user.name ${this.Authenticated}`
-        // return this.$store.state.user.name
+        
+        return this.$store.state.user.name
       }
       return ''
     }
-    /*
-    adopter_token_helper () {
-      // Objective: Give user feedback about signin status
-      // Stratgey: use the adopter name stashed in adopter token
-      // Strategy: use the adopter's identity key to color code drain symbols
-      // console.log('this.$store', this.$store);
-      // console.log('this.$store.state',this.$store.state);
-      // console.log('this.$store.state.token',this.$store.state.token);
-
-      return new Token Helper(this.$store.state.token)
-    },
-    is Authenticated () {
-      return new Token Helper(this.$store.state.token).is Authenticated();
-    }
-    */
-    /*
-    is Authenticated () {
-      if (this.$store.state.adopter.expires_at < new Date().getTime()) {
-        this.$store.commit('detoken')
-      }
-      return this.$store.state.adopter.authenticated
-    },
-    */
+ 
   }
 }
 </script>
